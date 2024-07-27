626f832c6ca5b147f756de6c076a3322a18add66
ASHMIT GHOSH <ashmitghosh14@gmail.com>
2024-07-27 23:13:53 +0530

    saving name changed

--- qrcode.py
+++ qrcode.py
@@ -2,7 +2,7 @@ import segno
 
 qrcode = segno.make_qr("https://www.youtube.com/playlist?list=PLxCzCOWd7aiGFBD2-2joCpWOLUrDLvVV_")
 qrcode.save(
-    "my_qrcode.png", 
+    "comp_networks.png", 
     scale=10,
     dark="green",
 )
